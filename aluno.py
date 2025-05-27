e605cec80435acccc6ed876daa5e0d873d4c278c
dudinha818 <rosa.santos18@escola.pr.gov.br>
2025-05-27 08:27:57 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,25 +1,26 @@
 import functools
 
 class Aluno:
-
     def __init__(self, nome, nota):
-
         self.nome = nome
-
         self.nota = nota
 
     def __repr__(self):
-
         return f'{self.nome}: {self.nota}'
 
-
-
 def comparar_alunos(a, b):
-
     return (a.nota > b.nota) - (a.nota < b.nota)
 
-alunos = [Aluno('Ana', 88), Aluno('Rafael', 92), Aluno('Paulo', 78)]
+# Lista para armazenar os alunos
+alunos = []
 
+# Solicitando informações ao usuário
+for _ in range(3):
+    nome = input("Informe o nome do aluno: ")
+    nota = int(input("Informe a nota do aluno: "))
+    alunos.append(Aluno(nome, nota))
+
+# Ordenando a lista de alunos
 alunos_ordenados = sorted(alunos, key=functools.cmp_to_key(comparar_alunos))
 
 print(alunos_ordenados)
